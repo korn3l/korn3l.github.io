324d0b9f42c0e672697d6d31837e5965b0e12ff7
Kornel Mika <129169565+korn3l@users.noreply.github.com>
2023-12-31 02:06:46 +0100

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,19 @@
+const modal = document.getElementById("SignUpPopUp");
+const btn = document.getElementById("SignButton");
+const btn2 = document.getElementById("SignButton2");
+
+btn.onclick = function () {
+  modal.style.display = "flex";
+  document.querySelector(".container").classList.toggle("container-blur");
+};
+
+btn2.onclick = function () {
+  modal.style.display = "none";
+  document.querySelector(".container").classList.toggle("container-blur");
+};
+
+window.onload = function () {
+  document.getElementById("SignButton2").onclick = function () {
+    window.location.href = "cards/products.html";
+  };
+};
